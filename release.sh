2f7fd7dd8fe54c0f95b22b0864450ab953af0d61
Mike Burr <mb@unintuitive.org>
2022-09-28 11:38:19 -0600

    Revert "wasm!!"
    This reverts commit 09d79418cf53ce7ce49b7903efe1dde1db9b4901.

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ue
-
-cargo build -q --release --target wasm32-unknown-unknown
-
-wasm-bindgen --out-dir pkg --target web --reference-types --no-typescript --omit-default-module-path \
-	     target/wasm32-unknown-unknown/release/index.wasm
-
-echo "The directory $(pwd) should now be usable as a complete wasm web app. Serve this directory and load \"index.htm\" in your web browser."
