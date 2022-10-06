026b8c474a0ce184e0cb1d62bb139075c62a08f9
Mike Burr <mb@unintuitive.org>
2022-10-06 12:58:26 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,7 @@ fn setup(
         ..Default::default()
     });
     commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
-            .looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 0.5, -1.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     }); //    let translation = Vec3::new(-2.0, 2.5, 5.0);
         //camera::spawn_camera(commands);
