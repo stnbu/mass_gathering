f14e3e0a9f0e8e6a0ba674f220ae7ea494844869
Mike Burr <mb@unintuitive.org>
2022-10-10 09:39:13 -0600

    space_camera -- broken parts and pieces

---
+++ src/space_camera.rs
@@ -0,0 +1,213 @@
+struct SpaceCamera;
+
+impl Plugin for SpaceCamera {
+    fn build(&self, app: &mut App) {
+        // app
+        //     .init_resource::<MyOtherResource>()
+        //     .add_event::<MyEvent>()
+        //     .add_startup_system(plugin_init)
+        //     .add_system(my_system);
+    }
+}
+
+// -----------------------
+
+use bevy::prelude::*;
+use std::f32::consts::TAU;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Curvature::default())
+        .add_state(AppState::Startup)
+        .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::on_update(AppState::Playing)
+                .with_system(rocket_forward)
+                .with_system(steer),
+        )
+        .add_system(bevy::window::close_on_esc) // "or prototyping" -- unclean shutdown
+        .add_system(handle_game_state)
+        .run();
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum AppState {
+    Startup,
+    Playing,
+    Paused,
+}
+
+#[derive(Debug, Default)]
+struct Curvature(Vec3);
+
+fn toggle_pause(current: &AppState) -> Option<AppState> {
+    match current {
+        AppState::Paused => Some(AppState::Playing),
+        AppState::Playing => Some(AppState::Paused),
+        _ => None,
+    }
+}
+
+fn handle_game_state(
+    mut focus_events: EventReader<bevy::window::WindowFocused>,
+    mut app_state: ResMut<State<AppState>>,
+    keys: Res<Input<KeyCode>>,
+    mouse_buttons: Res<Input<MouseButton>>,
+) {
+    let mut poked = false; // space bar hit or window left-clicked
+    for key in keys.get_just_pressed() {
+        if *key == KeyCode::Space {
+            poked = !poked;
+        }
+    }
+    if mouse_buttons.any_just_pressed([MouseButton::Left, MouseButton::Right]) {
+        poked = !poked;
+    }
+
+    if !poked {
+        for ev in focus_events.iter() {
+            if ev.focused {
+                app_state.overwrite_set(AppState::Playing).unwrap();
+            } else {
+                app_state.overwrite_set(AppState::Paused).unwrap();
+            }
+        }
+    } else {
+        if *(app_state.current()) == AppState::Startup {
+            app_state.overwrite_set(AppState::Playing).unwrap();
+        } else {
+            if let Some(new_state) = toggle_pause(app_state.current()) {
+                app_state.overwrite_set(new_state).unwrap();
+            }
+        }
+    }
+}
+
+fn rocket_forward(mut camera_query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
+    //time.delta();
+    let mut transform = camera_query.single_mut();
+    let direction = transform.local_z();
+    transform.translation -= direction * time.delta_seconds();
+}
+
+fn steer(
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Camera>>,
+    mut curvature: ResMut<Curvature>,
+) {
+    let gain = 0.2;
+    let nudge = TAU / 10000.0;
+    let mut roll = 0.0;
+    let mut pitch = 0.0;
+    let mut yaw = 0.0;
+    let mut had_input = false;
+    for key in keys.get_pressed() {
+        match key {
+            KeyCode::Left => {
+                roll += nudge * (curvature.0.z + 1.0);
+                had_input = true;
+                curvature.0.z += gain;
+            }
+            KeyCode::Right => {
+                roll -= nudge * (curvature.0.z + 1.0);
+                had_input = true;
+                curvature.0.z += gain;
+            }
+            KeyCode::Up => {
+                pitch -= nudge * (curvature.0.x + 1.0);
+                had_input = true;
+                curvature.0.x += gain;
+            }
+            KeyCode::Down => {
+                pitch += nudge * (curvature.0.x + 1.0);
+                had_input = true;
+                curvature.0.x += gain;
+            }
+            KeyCode::Z => {
+                yaw += nudge * (curvature.0.y + 1.0);
+                had_input = true;
+                curvature.0.y += gain;
+            }
+            KeyCode::X => {
+                yaw -= nudge * (curvature.0.y + 1.0);
+                had_input = true;
+                curvature.0.y += gain;
+            }
+            _ => (),
+        }
+    }
+    if !had_input {
+        if curvature.0.x > 0.0 {
+            curvature.0.x -= gain;
+            if curvature.0.x < 0.0 {
+                curvature.0.x = 0.0;
+            }
+        }
+        if curvature.0.y > 0.0 {
+            curvature.0.y -= gain;
+            if curvature.0.y < 0.0 {
+                curvature.0.y = 0.0;
+            }
+        }
+        if curvature.0.z > 0.0 {
+            curvature.0.z -= gain;
+            if curvature.0.z < 0.0 {
+                curvature.0.z = 0.0;
+            }
+        }
+    }
+    let mut transform = query.single_mut();
+    if roll != 0.0 || pitch != 0.0 || yaw != 0.0 {
+        let local_x = transform.local_x();
+        let local_y = transform.local_y();
+        let local_z = transform.local_z();
+        // Oh, I bet I need some math here.
+        transform.rotate(Quat::from_axis_angle(local_x, pitch));
+        transform.rotate(Quat::from_axis_angle(local_z, roll));
+        transform.rotate(Quat::from_axis_angle(local_y, yaw));
+    }
+}
+
+use rand::Rng;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mut rng = rand::thread_rng();
+    //let ran = || rng.gen::<f32>();
+    for x in 0..10 {
+        for y in 0..10 {
+            for z in 0..10 {
+                let x = (x * 4) as f32 - 5.0 + rng.gen::<f32>();
+                let y = (y * 4) as f32 - 5.0 + rng.gen::<f32>();
+                let z = (z * 4) as f32 - 5.0 + rng.gen::<f32>();
+                let r = rng.gen::<f32>();
+                let g = rng.gen::<f32>();
+                let b = rng.gen::<f32>();
+                commands.spawn_bundle(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube {
+                        size: rng.gen::<f32>() + 0.5,
+                    })),
+                    material: materials.add(Color::rgb(r, g, b).into()),
+                    transform: Transform::from_xyz(x, y, z),
+                    ..Default::default()
+                });
+            }
+        }
+    }
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_xyz(30.0, 30.0, 30.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 0.0).looking_at(
+            Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>()),
+            Vec3::Y,
+        ),
+        ..Default::default()
+    });
+}
