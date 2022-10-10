fd9b31a72abfafe51db0c09dfb2b552bf3da104a
Mike Burr <mb@unintuitive.org>
2022-10-10 13:56:04 -0600

    back to peak monocam

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 use bevy::prelude::*;
-use rand::Rng;
-
-mod space_camera;
+use std::f32::consts::TAU;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(Curvature::default())
         .add_state(AppState::Startup)
+        .add_startup_system(setup)
         .add_system_set(
             SystemSet::on_update(AppState::Playing)
-                .with_system(space_camera::move_forward)
-                .with_system(space_camera::steer),
+                .with_system(rocket_forward)
+                .with_system(steer),
         )
-        .insert_resource(space_camera::CameraConfig {
-            transform: Transform::from_translation(Vec3::new(40.0, 40.0, 40.0))
-                .looking_at(Vec3::new(1.0, 1.0, 1.0), Vec3::Y),
-        })
-        .add_plugin(space_camera::SpaceCamera)
-        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc) // "or prototyping" -- unclean shutdown
         .add_system(handle_game_state)
         .run();
@@ -29,6 +23,9 @@ enum AppState {
     Playing,
     Paused,
 }
+
+#[derive(Debug, Default)]
+struct Curvature(Vec3);
 
 fn toggle_pause(current: &AppState) -> Option<AppState> {
     match current {
@@ -73,6 +70,93 @@ fn handle_game_state(
     }
 }
 
+fn rocket_forward(mut camera_query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
+    //time.delta();
+    let mut transform = camera_query.single_mut();
+    let direction = transform.local_z();
+    transform.translation -= direction * time.delta_seconds();
+}
+
+fn steer(
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Camera>>,
+    mut curvature: ResMut<Curvature>,
+) {
+    let gain = 0.2;
+    let nudge = TAU / 10000.0;
+    let mut roll = 0.0;
+    let mut pitch = 0.0;
+    let mut yaw = 0.0;
+    let mut had_input = false;
+    for key in keys.get_pressed() {
+        match key {
+            KeyCode::Left => {
+                roll += nudge * (curvature.0.z + 1.0);
+                had_input = true;
+                curvature.0.z += gain;
+            }
+            KeyCode::Right => {
+                roll -= nudge * (curvature.0.z + 1.0);
+                had_input = true;
+                curvature.0.z += gain;
+            }
+            KeyCode::Up => {
+                pitch -= nudge * (curvature.0.x + 1.0);
+                had_input = true;
+                curvature.0.x += gain;
+            }
+            KeyCode::Down => {
+                pitch += nudge * (curvature.0.x + 1.0);
+                had_input = true;
+                curvature.0.x += gain;
+            }
+            KeyCode::Z => {
+                yaw += nudge * (curvature.0.y + 1.0);
+                had_input = true;
+                curvature.0.y += gain;
+            }
+            KeyCode::X => {
+                yaw -= nudge * (curvature.0.y + 1.0);
+                had_input = true;
+                curvature.0.y += gain;
+            }
+            _ => (),
+        }
+    }
+    if !had_input {
+        if curvature.0.x > 0.0 {
+            curvature.0.x -= gain;
+            if curvature.0.x < 0.0 {
+                curvature.0.x = 0.0;
+            }
+        }
+        if curvature.0.y > 0.0 {
+            curvature.0.y -= gain;
+            if curvature.0.y < 0.0 {
+                curvature.0.y = 0.0;
+            }
+        }
+        if curvature.0.z > 0.0 {
+            curvature.0.z -= gain;
+            if curvature.0.z < 0.0 {
+                curvature.0.z = 0.0;
+            }
+        }
+    }
+    let mut transform = query.single_mut();
+    if roll != 0.0 || pitch != 0.0 || yaw != 0.0 {
+        let local_x = transform.local_x();
+        let local_y = transform.local_y();
+        let local_z = transform.local_z();
+        // Oh, I bet I need some math here.
+        transform.rotate(Quat::from_axis_angle(local_x, pitch));
+        transform.rotate(Quat::from_axis_angle(local_z, roll));
+        transform.rotate(Quat::from_axis_angle(local_y, yaw));
+    }
+}
+
+use rand::Rng;
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -102,6 +186,14 @@ fn setup(
     }
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_xyz(30.0, 30.0, 30.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 0.0).looking_at(
+            Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>()),
+            Vec3::Y,
+        ),
         ..Default::default()
     });
 }
