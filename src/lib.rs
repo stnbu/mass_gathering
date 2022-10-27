6c1b3f7a9240813a8fab766583df8fbe67b6cf19
Mike Burr <mb@unintuitive.org>
2022-10-27 13:26:10 -0600

    y-libified

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use bevy_rapier3d::prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin};
+use rand::Rng;
+use std::f32::consts::TAU;
+
+mod physics;
+use physics::*;
+
+mod craft;
+use craft::*;
+
+// PSA: see examples/3d/split_screen.rs
+
+pub struct Game;
+
+impl Plugin for Game {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(ClearColor(Color::MIDNIGHT_BLUE * 0.1))
+            .add_plugins(DefaultPlugins)
+            .add_plugin(EguiPlugin)
+            .add_state(AppState::Startup)
+            .add_system_set(
+                SystemSet::on_update(AppState::Playing)
+                    .with_system(move_forward)
+                    .with_system(steer)
+                    .with_system(handle_projectile_engagement)
+                    .with_system(handle_projectile_flight)
+                    .with_system(animate_projectile_explosion),
+            )
+            .add_system_set(
+                SystemSet::on_update(AppState::Placeholder)
+                    .with_system(collision_events)
+                    .with_system(freefall),
+            )
+            .add_startup_system(setup)
+            .add_startup_system(spacecraft_setup)
+            .add_system(bevy::window::close_on_esc)
+            .add_system(handle_game_state)
+            .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+            .add_system(hud);
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Copy)]
+enum AppState {
+    Startup,
+    Playing,
+    Paused,
+    Menu,
+    Placeholder,
+}
+
+fn handle_game_state(mut app_state: ResMut<State<AppState>>, keys: Res<Input<KeyCode>>) {
+    use AppState::*;
+    use KeyCode::*;
+    let next_state =
+        keys.get_just_pressed()
+            .fold(None, |_state, key| match (*app_state.current(), *key) {
+                (Playing, Space) => Some(Paused),
+                (Paused, Space) => Some(Playing),
+                (Menu, M) => Some(Playing),
+                (_, M) => Some(Menu),
+                (Startup, _) => Some(Playing),
+                _ => None,
+            });
+    if let Some(state) = next_state {
+        let _ = app_state.overwrite_set(state);
+    }
+}
+
+// Take the latitude (poles are [1,-1]) and the longitude (portion around, starting at (0,0,1))
+// and return the x, y, z on the unit sphere.
+fn latlon_to_cartesian(lat: f32, lon: f32) -> Vec3 {
+    let theta = (lat * 2.0 - 1.0).acos(); // latitude. -1 & 1 are poles. 0 is equator.
+    let phi = lon * TAU; // portion around the planet `[0,1)` (from Greenwich)
+    let x = theta.sin() * phi.cos();
+    let y = theta.sin() * phi.sin();
+    let z = theta.cos();
+    Vec3::new(x, y, z)
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut rapier_config: ResMut<RapierConfiguration>,
+) {
+    rapier_config.gravity = Vec3::ZERO;
+
+    let mut rng = rand::thread_rng();
+    let mut rf = || rng.gen::<f32>();
+    let pair_count = 40;
+    for _ in 0..pair_count {
+        let position = latlon_to_cartesian(rf(), rf()) * (rf() * 40.0 + 10.0);
+        let velocity = latlon_to_cartesian(rf(), rf()) * Vec3::new(10.0, rf() * 0.1, 10.0);
+        let radius = rf() + 2.0;
+        for side in [-1.0, 1.0] {
+            let color = Color::rgb(rf(), rf(), rf());
+            spawn_planet(
+                radius,
+                position * side,
+                velocity * side,
+                color,
+                &mut commands,
+                &mut meshes,
+                &mut materials,
+            );
+        }
+    }
+}
