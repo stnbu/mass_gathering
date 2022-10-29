41d62e822c8ff8d38685f70a1088b241cdae56fc
Mike Burr <mb@unintuitive.org>
2022-10-29 10:37:03 -0600

    logging

--- examples/shooting_gallery.rs
+++ examples/shooting_gallery.rs
@@ -1,29 +1,37 @@
+use bevy::log::LogSettings;
+
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin};
 use mass_gathering::prelude::*;
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(Color::WHITE * 0.8))
+    let mut app = App::new();
+    app.insert_resource(ClearColor(Color::WHITE * 0.8))
         .insert_resource(SpaceCraftConfig {
             show_debug_markers: true,
             show_impact_explosions: false,
             projectile_radius: 0.02,
-        })
-        .add_plugins(DefaultPlugins)
-        .add_system(move_forward)
-        .add_system(steer)
-        .add_system(handle_projectile_engagement)
-        .add_system(handle_projectile_flight)
-        .add_system(animate_projectile_explosion)
-        .add_startup_system(setup)
-        .add_startup_system(spacecraft_setup)
-        .add_system(freefall)
-        .add_system(collision_events)
-        .add_system(timer_despawn)
-        .add_system(do_blink)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .run();
+        });
+
+    app.insert_resource(LogSettings {
+        filter: "debug".into(),
+        level: bevy::log::Level::DEBUG,
+    })
+    .add_plugins(DefaultPlugins)
+    .add_system(move_forward)
+    .add_system(steer)
+    .add_system(handle_projectile_engagement)
+    .add_system(handle_projectile_flight)
+    .add_system(animate_projectile_explosion)
+    .add_startup_system(setup)
+    .add_startup_system(spacecraft_setup)
+    .add_system(freefall)
+    .add_system(collision_events)
+    .add_system(timer_despawn)
+    .add_system(do_blink)
+    .add_plugin(RapierPhysicsPlugin::<NoUserData>::default());
+
+    app.run();
 }
 
 fn setup(
