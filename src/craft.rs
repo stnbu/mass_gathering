e549408ae14edf34350b72a7c4d8a8fb5a4c5763
Mike Burr <mb@unintuitive.org>
2022-10-31 11:02:29 -0600

    .

--- src/craft.rs
+++ src/craft.rs
@@ -371,8 +371,7 @@ pub fn handle_projectile_engagement(
                     debug!("Firing projectile!");
                     let global_impact_site = ray_origin + (ray_direction * distance);
                     println!("{:?}", planet_transform.scale);
-                    let local_impact_site = (global_impact_site - planet_transform.translation)
-                        / planet_transform.scale.length();
+                    let local_impact_site = global_impact_site - planet_transform.translation;
                     if config.show_debug_markers {
                         let planet_local_marker = commands
                             .spawn_bundle(PbrBundle {
