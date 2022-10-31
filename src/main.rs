62a029a66f8bf436b92ac526b6b589ec088087eb
Mike Burr <mb@unintuitive.org>
2022-10-30 22:35:29 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use bevy::prelude::App;
-use mass_gathering::prelude::my_planets;
+use mass_gathering::prelude::{my_planets, SpacecraftConfig};
 use mass_gathering::FullGame;
 
 fn main() {
     App::new()
+        .insert_resource(SpacecraftConfig {
+            stereo_enabled: false,
+            stereo_iod: -2.0,
+            ..Default::default()
+        })
         .add_plugins(FullGame)
         .add_startup_system(my_planets)
         .run();
