51d9b103781b916afea2e2ce09fcd212b70aef96
Mike Burr <mb@unintuitive.org>
2022-11-09 14:26:11 -0700

    calculon is back

--- examples/despawned_planet_cruft_bugs.rs
+++ examples/despawned_planet_cruft_bugs.rs
@@ -22,8 +22,11 @@ fn main() {
         })
         .add_plugins(FullGame)
         .add_startup_system(setup)
+        .add_system(replace_ol_blue)
         .run();
 }
+
+//use mass_gathering::prelude::DespawnPlanetEvent;
 
 fn setup(
     mut commands: Commands,
@@ -42,6 +45,12 @@ fn setup(
     As of 53abb5f5ae0092812b7548ed46d5ed349df05d27
       * A crash has been observed!
       * In-flight projectiles halt in flight and instead (!) log a warning.
+
+    [update]
+    As of fa9101033a220dac8323b919e73da3f7a5c44d31
+      * Despite the only place planets get despawned coming _after_ `move_projectile` we still get "orphaned" projectiles e.g.
+          > While moving projectile: planet 10v0 not found
+      * No sign of crash so far. No crashing. That means I cannot get it to crash now with this example.
     */
 
     spawn_planet(
@@ -62,4 +71,23 @@ fn setup(
         &mut meshes,
         &mut materials,
     );
+}
+
+fn replace_ol_blue(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    despawn_planet_events: EventReader<DespawnPlanetEvent>,
+) {
+    if !despawn_planet_events.is_empty() {
+        spawn_planet(
+            9.0,
+            Vec3::Z * 30.0,
+            Vec3::Z * -2.0,
+            Color::BLUE,
+            &mut commands,
+            &mut meshes,
+            &mut materials,
+        );
+    }
 }
