4df34ef93b0b7a8018f7f2466fcb6c71b376b55f
Mike Burr <mb@unintuitive.org>
2022-11-12 14:50:18 -0700

    YY

--- src/helpscreen.rs
+++ src/helpscreen.rs
@@ -52,89 +52,20 @@ pub fn helpscreen(mut ctx: ResMut<EguiContext>) {
             ..Default::default()
         })
         .show(ctx.ctx_mut(), |ui| {
-            TableBuilder::new(ui)
-                .striped(false)
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                .column(Size::initial(120.0))
-                .column(Size::initial(270.0))
-                .column(Size::remainder())
-                .resizable(false)
-                .header(50.0, |mut header| {
-                    header.col(|ui| {
-                        styled_text_label(22.0, ui, "Key");
+            use egui_extras::StripBuilder;
+            StripBuilder::new(ui)
+                .size(Size::exact(65.0))
+                .size(Size::exact(30.0))
+                .size(Size::remainder())
+                .vertical(|mut strip| {
+                    strip.cell(|ui| {
+                        styled_text_label(50.0, ui, "CLICK ANYWHERE TO BEGIN!");
                     });
-                    header.col(|ui| {
-                        styled_text_label(22.0, ui, "Mouse");
+                    strip.cell(|ui| {
+                        styled_text_label(22.0, ui, ".. Input Bindings ..");
                     });
-                    header.col(|ui| {
-                        styled_text_label(20.0, ui, "Function");
-                    });
-                })
-                .body(|mut body| {
-                    let row_height = 22.0;
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Space");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Left click");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Fire Projectile");
-                        });
-                    });
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "W / S");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Up / Down");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Pitch up / Pitch down");
-                        });
-                    });
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "A / D");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Left / Right");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Yaw left / Yaw right");
-                        });
-                    });
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Z / X");
-                        });
-                        row.col(|_| {});
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Roll left / Roll right");
-                        });
-                    });
-
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "PgUp / PgDn");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Scroll wheel");
-                        });
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "Speed plus / Speed minus");
-                        });
-                    });
-
-                    body.row(row_height, |mut row| {
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "H or P");
-                        });
-                        row.col(|_| {});
-                        row.col(|ui| {
-                            styled_text_label(18.0, ui, "[P]ause and show this [H]elp screen");
-                        });
+                    strip.cell(|ui| {
+                        build_table(ui);
                     });
                 });
         });
@@ -145,4 +76,92 @@ fn styled_text_label(height: f32, ui: &mut egui::Ui, text: &str) {
         size: height,
         family: Monospace,
     }));
+}
+
+fn build_table(ui: &mut egui::Ui) {
+    TableBuilder::new(ui)
+        .striped(false)
+        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+        .column(Size::initial(120.0))
+        .column(Size::initial(270.0))
+        .column(Size::remainder())
+        .resizable(false)
+        .header(50.0, |mut header| {
+            header.col(|ui| {
+                styled_text_label(22.0, ui, "Key");
+            });
+            header.col(|ui| {
+                styled_text_label(22.0, ui, "Mouse");
+            });
+            header.col(|ui| {
+                styled_text_label(20.0, ui, "Function");
+            });
+        })
+        .body(|mut body| {
+            let row_height = 22.0;
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Space");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Left click");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Fire Projectile");
+                });
+            });
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "W / S");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Up / Down");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Pitch up / Pitch down");
+                });
+            });
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "A / D");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Left / Right");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Yaw left / Yaw right");
+                });
+            });
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Z / X");
+                });
+                row.col(|_| {});
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Roll left / Roll right");
+                });
+            });
+
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "PgUp / PgDn");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Scroll wheel");
+                });
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "Speed plus / Speed minus");
+                });
+            });
+
+            body.row(row_height, |mut row| {
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "H or P");
+                });
+                row.col(|_| {});
+                row.col(|ui| {
+                    styled_text_label(18.0, ui, "[P]ause and show this [H]elp screen");
+                });
+            });
+        });
 }
