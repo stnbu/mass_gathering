44e82ea2e4b2da6ecf6fe4f1803ceb5550304e11
Mike Burr <mb@unintuitive.org>
2022-11-13 15:55:50 -0700

    y

--- examples/vector_ball.rs
+++ examples/vector_ball.rs
@@ -16,14 +16,14 @@ const BALL_RADIUS: f32 = 3.5;
 const FLOAT_HEIGHT: f32 = 2.0;
 
 const VECTOR_LENGTH: f32 = 14.0;
-const R: f32 = 1.0;
+const VECTOR_SCALE: f32 = 1.0;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let S = VECTOR_LENGTH - BALL_RADIUS - FLOAT_HEIGHT - 2.0 * R;
+    let vector_cylinder_length = VECTOR_LENGTH - BALL_RADIUS - FLOAT_HEIGHT - 2.0 * VECTOR_SCALE;
     commands
         .spawn_bundle(SpatialBundle::default())
         .with_children(|child| {
@@ -41,27 +41,31 @@ fn setup(
             child.spawn_bundle(PbrBundle {
                 mesh: meshes.add(
                     (Cone {
-                        radius: 2.0 * R,
-                        height: 2.0 * R,
+                        radius: 2.0 * VECTOR_SCALE,
+                        height: 2.0 * VECTOR_SCALE,
                         ..Default::default()
                     })
                     .into(),
                 ),
-                transform: Transform::from_xyz(0.0, VECTOR_LENGTH - 2.0 * R, 0.0),
+                transform: Transform::from_xyz(0.0, VECTOR_LENGTH - 2.0 * VECTOR_SCALE, 0.0),
                 material: materials.add(Color::GREEN.into()),
                 ..Default::default()
             });
             child.spawn_bundle(PbrBundle {
                 mesh: meshes.add(
                     (Cylinder {
-                        height: VECTOR_LENGTH - BALL_RADIUS - FLOAT_HEIGHT - 2.0 * R,
-                        radius_bottom: R,
-                        radius_top: R,
+                        height: vector_cylinder_length,
+                        radius_bottom: VECTOR_SCALE,
+                        radius_top: VECTOR_SCALE,
                         ..Default::default()
                     })
                     .into(),
                 ),
-                transform: Transform::from_xyz(0.0, S * 0.5 + BALL_RADIUS + FLOAT_HEIGHT, 0.0),
+                transform: Transform::from_xyz(
+                    0.0,
+                    vector_cylinder_length * 0.5 + BALL_RADIUS + FLOAT_HEIGHT,
+                    0.0,
+                ),
                 material: materials.add(Color::GREEN.into()),
                 ..Default::default()
             });
