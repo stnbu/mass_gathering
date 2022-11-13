c35b8b9381ab3b2be55ffe8af2e6a2210d87e891
Mike Burr <mb@unintuitive.org>
2022-11-13 15:15:43 -0700

    y-vec-bal

--- examples/vector_ball.rs
+++ examples/vector_ball.rs
@@ -4,6 +4,10 @@ use mass_gathering::prelude::*;
 fn main() {
     App::new()
         .add_plugins(FullGame)
+        .insert_resource(SpacecraftConfig {
+            start_transform: Transform::from_xyz(0.0, 0.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
         .add_startup_system(setup)
         .run();
 }
@@ -13,6 +17,7 @@ const R: f32 = 1.0;
 const B: f32 = 2.0;
 const I: f32 = 3.5;
 
+use std::f32::consts::PI;
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -20,15 +25,32 @@ fn setup(
 ) {
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(
-            (Cone {
-                radius: 2.0 * R,
-                height: 2.0 * R,
+            (shape::Icosphere {
+                radius: 0.2 * R,
                 ..Default::default()
             })
             .into(),
         ),
-        transform: Transform::from_xyz(0.0, 0.0, -5.0),
-        material: materials.add(Color::GREEN.into()),
+        material: materials.add(Color::WHITE.into()),
         ..Default::default()
     });
+
+    commands
+        .spawn_bundle(SpatialBundle::default())
+        .with_children(|child| {
+            child.spawn_bundle(PbrBundle {
+                mesh: meshes.add(
+                    (Cone {
+                        radius: 2.0 * R,
+                        height: 2.0 * R,
+                        ..Default::default()
+                    })
+                    .into(),
+                ),
+                transform: Transform::from_rotation(Quat::from_rotation_x(PI))
+                    .with_translation(Vec3::Y * R),
+                material: materials.add(Color::GREEN.into()),
+                ..Default::default()
+            });
+        });
 }
