a40d8794d3d0f9ef89dc54ef5088867b00ff65c1
Mike Burr <mb@unintuitive.org>
2022-11-25 12:39:19 -0700

    craft conf needs/gets its own file; bump vers `cuz

--- src/craft/mod.rs
+++ src/craft/mod.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy::transform::TransformBundle;
 
 use bevy_rapier3d::prelude::{
     ActiveEvents, Collider, QueryFilter, RapierContext, RigidBody, Sensor,
@@ -9,6 +8,9 @@ use std::collections::HashSet;
 
 mod controls;
 pub use controls::*;
+
+mod setup;
+pub use setup::*;
 
 use crate::mass_to_radius;
 use crate::physics::Momentum;
@@ -90,102 +92,6 @@ use crate::mg_shapes::*;
 
 #[derive(Component)]
 pub struct VectorBallTransform;
-
-pub fn spacecraft_setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    config: Res<SpacecraftConfig>,
-) {
-    let spacecraft = commands
-        .spawn_bundle(TransformBundle::from_transform(config.start_transform))
-        .insert_bundle(VisibilityBundle::default())
-        .insert(Spacecraft {
-            speed: config.start_speed,
-        })
-        .with_children(|child| {
-            child.spawn_bundle(Camera3dBundle {
-                transform: Transform::from_xyz(0.0, 0.0, 0.0).looking_at(-Vec3::Z, Vec3::Y),
-                ..default()
-            });
-            // Possibly the worst way to implement "crosshairs" evar.
-            child
-                .spawn_bundle(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Icosphere {
-                        radius: 0.01,
-                        ..Default::default()
-                    })),
-                    material: materials.add(Color::LIME_GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -7.0),
-                    visibility: Visibility { is_visible: false },
-                    ..Default::default()
-                })
-                .insert(SpacecraftAR::CrosshairsCold);
-            child
-                .spawn_bundle(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Box::new(0.005, 5.0, 0.08))),
-                    material: materials.add(Color::GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -7.0),
-                    visibility: Visibility { is_visible: false },
-                    ..Default::default()
-                })
-                .insert(SpacecraftAR::CrosshairsHot);
-            child
-                .spawn_bundle(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Box::new(5.0, 0.005, 0.08))),
-                    material: materials.add(Color::GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -6.0),
-                    visibility: Visibility { is_visible: false },
-                    ..Default::default()
-                })
-                .insert(SpacecraftAR::CrosshairsHot);
-
-            // Various lights for seeing
-            child.spawn_bundle(PointLightBundle {
-                transform: Transform::from_xyz(10.0, -10.0, -25.0),
-                point_light: PointLight {
-                    intensity: 5000.0 * 1.7,
-                    range: 1000.0,
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-            child.spawn_bundle(PointLightBundle {
-                transform: Transform::from_xyz(-10.0, 5.0, -35.0),
-                point_light: PointLight {
-                    intensity: 5000.0 * 1.5,
-                    range: 1000.0,
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-            child.spawn_bundle(PointLightBundle {
-                transform: Transform::from_xyz(30.0, -20.0, 80.0),
-                point_light: PointLight {
-                    intensity: 1000000.0 * 0.7,
-                    range: 1000.0,
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-            child.spawn_bundle(PointLightBundle {
-                transform: Transform::from_xyz(-30.0, 10.0, 100.0),
-                point_light: PointLight {
-                    intensity: 1000000.0 * 0.8,
-                    range: 1000.0,
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-            child
-                .spawn_bundle(TransformBundle::from_transform(Transform::from_xyz(
-                    -0.12, -0.06, -0.25,
-                )))
-                .insert(VectorBallTransform);
-        })
-        .id();
-    debug!("Spawned spacecraft with entity {spacecraft:?}");
-}
 
 pub fn set_ar_default_visibility(mut ar_query: Query<(&mut Visibility, &SpacecraftAR)>) {
     for (mut visibility, mode) in ar_query.iter_mut() {
