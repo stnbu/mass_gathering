9e7d7fea66ee320abd8af20022cf312dda61ed04
Mike Burr <mb@unintuitive.org>
2022-11-28 15:27:04 -0700

    y

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -34,14 +34,35 @@ struct ClientLobby {
     players: HashMap<u64, PlayerInfo>,
 }
 
+//
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    id: String,
+    #[arg(short, long, default_value_t = 1)]
+    value: u64,
+}
+// fn main() {
+//     let args = Args::parse();
+//     for _ in 0..args.value {
+//         println!("Hello {}!", args.id)
+//     }
+// }
+//
+
 fn new_renet_client() -> RenetClient {
+    let client_id = Args::parse().value;
+
     let server_addr = format!("{SERVER_ADDR}:{PORT_NUMBER}").parse().unwrap();
     let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
     let connection_config = client_connection_config();
     let current_time = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap();
-    let client_id = current_time.as_millis() as u64;
+    //let client_id = current_time.as_millis() as u64;
+    //let client_id =
     let authentication = ClientAuthentication::Unsecure {
         client_id,
         protocol_id: PROTOCOL_ID,
