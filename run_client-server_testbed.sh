709b59fc72906e4e321ddf29a754748c8ac18239
Mike Burr <mb@unintuitive.org>
2022-11-29 13:54:15 -0700

    discard scafold junk

--- run_client-server_testbed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -u
-
-for ID in 49; do
-    echo ">>> Running client ${ID} \"in the background\"..." >&2
-    RUST_LOG=error cargo run --bin client -- --id $ID &
-done &
-echo ">>> Running server \"in the forground\"..." >&2
-cargo run --bin server
