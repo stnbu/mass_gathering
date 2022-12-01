1446a1c05d5cc3809ca30f1b6ab476be2f5618fb
Mike Burr <mb@unintuitive.org>
2022-12-01 09:35:52 -0700

    y~weird-but-close

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -9,7 +9,7 @@ fn planets(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for n in [(0.0, 0.0, 0.0)] {
+    for n in [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)] {
         let (a, b, c) = n;
         commands.spawn(PointMassBundle {
             pbr: PbrBundle {
@@ -17,7 +17,7 @@ fn planets(
                     radius: 1.0,
                     ..Default::default()
                 })),
-                material: materials.add(Color::rgb(1.0, b, c).into()),
+                material: materials.add(Color::rgb(a, b, c).into()),
                 transform: Transform::from_xyz(a * 6.0, b * 6.0, c * 6.0),
                 ..Default::default()
             },
