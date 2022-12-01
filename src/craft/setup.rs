37a2a29890091cb0b0f3a7b2c6fada0224a6ace9
Mike Burr <mb@unintuitive.org>
2022-12-01 11:41:32 -0700

    meh

--- src/craft/setup.rs
+++ src/craft/setup.rs
@@ -28,32 +28,43 @@ pub fn spacecraft_setup(
                 ..Default::default()
             });
             // Possibly the worst way to implement "crosshairs" evar.
+            //
+            // This coefficient to make the crosshairs "as close as possible" to our "eyeball"
+            let distance = 0.05;
             child
                 .spawn(PbrBundle {
                     mesh: meshes.add(Mesh::from(shape::Icosphere {
-                        radius: 0.01,
+                        radius: 0.01 * distance,
                         ..Default::default()
                     })),
                     material: materials.add(Color::LIME_GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -7.0),
+                    transform: Transform::from_xyz(0.0, 0.0, -7.0 * distance),
                     visibility: Visibility { is_visible: false },
                     ..Default::default()
                 })
                 .insert(SpacecraftAR::CrosshairsCold);
             child
                 .spawn(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Box::new(0.005, 5.0, 0.08))),
+                    mesh: meshes.add(Mesh::from(shape::Box::new(
+                        0.005 * distance,
+                        5.0 * distance,
+                        0.08 * distance,
+                    ))),
                     material: materials.add(Color::GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -7.0),
+                    transform: Transform::from_xyz(0.0, 0.0, -7.0 * distance),
                     visibility: Visibility { is_visible: false },
                     ..Default::default()
                 })
                 .insert(SpacecraftAR::CrosshairsHot);
             child
                 .spawn(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Box::new(5.0, 0.005, 0.08))),
+                    mesh: meshes.add(Mesh::from(shape::Box::new(
+                        5.0 * distance,
+                        0.005 * distance,
+                        0.08 * distance,
+                    ))),
                     material: materials.add(Color::GREEN.into()),
-                    transform: Transform::from_xyz(0.0, 0.0, -6.0),
+                    transform: Transform::from_xyz(0.0, 0.0, -6.0 * distance),
                     visibility: Visibility { is_visible: false },
                     ..Default::default()
                 })
