8f0de5d2412ec1f104244c79e3931e62d0f5123a
Mike Burr <mb@unintuitive.org>
2022-12-04 14:25:27 -0700

    .

--- run_client-server_testbed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -u
-
-for ID in 49 ; do
-    echo ">>> Running client ${ID} \"in the background\"..." >&2
-    cargo run --bin prototype-client -- --id $ID &
-done &
-echo ">>> Running server \"in the forground\"..." >&2
-cargo run --bin server
