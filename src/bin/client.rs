da390e9f791258f1fe5db14e771728c107fe8d69
Mike Burr <mb@unintuitive.org>
2022-12-08 09:37:23 -0700

    .

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -13,13 +13,15 @@ fn main() {
     App::new()
         .add_event::<ClientMessages>()
         .insert_resource(PhysicsConfig { sims_per_frame: 5 })
-        .add_plugin(FullGame::Client)
         .add_startup_system(spawn_arena_view_camera)
+        .add_plugin(FullGame::Client)
+        //
         .add_plugin(RenetClientPlugin::default())
         .add_system(client_sync_players.with_run_criteria(run_if_client_connected))
         .add_system(send_client_messages.with_run_criteria(run_if_client_connected))
         .add_system(panic_on_error_system)
         .add_system_set(SystemSet::on_update(GameState::Stopped).with_system(client_menu))
+        //
         .run();
 }
 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -15,12 +15,14 @@ fn main() {
     App::new()
         .init_resource::<InitData>()
         .insert_resource(PhysicsConfig { sims_per_frame: 5 })
-        .add_plugin(FullGame::Server)
         .add_startup_system(spawn_arena_view_camera)
+        .add_plugin(FullGame::Server)
+        //
         .add_startup_system(cubic)
         .add_plugin(RenetServerPlugin::default())
         .insert_resource(new_renet_server())
         .add_system(handle_server_events)
+        //
         .run();
 }
 
