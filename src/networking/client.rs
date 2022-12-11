8204cee324bd6d9a05bfe1dc579f8b507e206d31
Mike Burr <mb@unintuitive.org>
2022-12-11 09:14:14 -0700

    y - needs to handle missing inhabitant

--- src/networking/client.rs
+++ src/networking/client.rs
@@ -1,8 +1,11 @@
 use crate::{systems::spawn_planet, GameConfig, GameState};
-use bevy::input::mouse::{MouseMotion, MouseWheel};
+use bevy::input::{
+    mouse::{MouseButton, MouseButtonInput, MouseMotion, MouseWheel},
+    ButtonState,
+};
 use bevy::prelude::*;
-use bevy::prelude::{EventReader, Local, Quat, Query, Res, Time, Transform, Vec3, With};
 use bevy_renet::renet::{ClientAuthentication, RenetClient, RenetConnectionConfig};
+use std::f32::consts::TAU;
 use std::{net::UdpSocket, time::SystemTime};
 
 use crate::networking::*;
@@ -129,36 +132,73 @@ pub fn set_window_title(
     window.set_title(format!("Client[{:?}] : nick={nick}", game_state.current()));
 }
 
-#[derive(Default)]
-pub struct AngularVelocity(Vec3);
-
 pub fn control(
+    keys: Res<Input<KeyCode>>,
     mut inhabitant_query: Query<&mut Transform, With<Inhabited>>,
     mut mouse_motion_events: EventReader<MouseMotion>,
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
     mut mouse_wheel_events: EventReader<MouseWheel>,
-    mut angular_velocity: Local<AngularVelocity>,
     time: Res<Time>,
 ) {
-    if let Ok(mut transform) = inhabitant_query.get_single_mut() {
-        let mouse_motion_scaling = 0.0005;
-        for event in mouse_motion_events.iter() {
-            angular_velocity.0.x += event.delta.y * mouse_motion_scaling;
-            angular_velocity.0.y += event.delta.x * mouse_motion_scaling;
-        }
-        let mouse_wheel_scaling = mouse_motion_scaling * 15.0;
-        for event in mouse_wheel_events.iter() {
-            angular_velocity.0.z += event.y * mouse_wheel_scaling;
-        }
+    let nudge = TAU / 10000.0;
+    let keys_scaling = 10.0;
+    let mouse_scaling = 0.0001;
 
-        let rotation = angular_velocity.0 * time.delta_seconds();
+    // rotation about local axes
+    let mut rotation = Vec3::ZERO;
 
-        let local_x = transform.local_x();
-        let local_y = transform.local_y();
-        let local_z = transform.local_z();
-        transform.rotate(Quat::from_axis_angle(local_x, rotation.x));
-        transform.rotate(Quat::from_axis_angle(local_y, rotation.y));
-        transform.rotate(Quat::from_axis_angle(local_z, rotation.z));
-    } else {
-        error!("Did not find inhabitant!");
+    let mut transform = inhabitant_query.get_single_mut().unwrap();
+
+    for key in keys.get_pressed() {
+        match key {
+            KeyCode::A => {
+                rotation.y += nudge;
+            }
+            KeyCode::D => {
+                rotation.y -= nudge;
+            }
+            KeyCode::W => {
+                rotation.x += nudge;
+            }
+            KeyCode::S => {
+                rotation.x -= nudge;
+            }
+            KeyCode::Z => {
+                rotation.z += nudge;
+            }
+            KeyCode::X => {
+                rotation.z -= nudge;
+            }
+            _ => (),
+        }
     }
+
+    for event in mouse_motion_events.iter() {
+        rotation.x -= event.delta.y * mouse_scaling;
+        rotation.y -= event.delta.x * mouse_scaling;
+    }
+    for MouseButtonInput { button, state } in mouse_button_input_events.iter() {
+        if *state == ButtonState::Pressed {
+            if *button == MouseButton::Left {
+                println!("LEFT CLICK!");
+            }
+            if *button == MouseButton::Right {
+                println!("RIGHT CLICK!");
+            }
+        }
+    }
+    let mouse_wheel_scaling = mouse_scaling * 15.0;
+    for event in mouse_wheel_events.iter() {
+        rotation.z += event.y * mouse_wheel_scaling;
+    }
+
+    let frame_time = time.delta_seconds() * 60.0;
+    rotation *= keys_scaling * frame_time;
+
+    let local_x = transform.local_x();
+    let local_y = transform.local_y();
+    let local_z = transform.local_z();
+    transform.rotate(Quat::from_axis_angle(local_x, rotation.x));
+    transform.rotate(Quat::from_axis_angle(local_z, rotation.z));
+    transform.rotate(Quat::from_axis_angle(local_y, rotation.y));
 }
