67ce997a2121232231788a51fd6533805917eab0
Mike Burr <mb@unintuitive.org>
2022-12-13 12:32:58 -0700

    .

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,15 @@
 use bevy::prelude::*;
+use clap::Parser;
 use mass_gathering::{networking::*, systems::*};
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = 10)]
+    speed: u32,
+}
+
 fn main() {
+    let _speed = Args::parse().speed;
     App::new()
         .insert_resource(testing_no_unhinhabited())
         .add_startup_system(spawn_arena_view_camera)
