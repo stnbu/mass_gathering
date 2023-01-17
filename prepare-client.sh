e766c992bfeb66a2a9c43190e132f4e9f95f83ba
Mike Burr <mb@unintuitive.org>
2023-01-17 09:11:30 -0700

    .

---
+++ prepare-client.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ux
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## clone...
+#git clone https://github.com/stnbu/mass_gathering.git ./mass_gathering
+#cd mass_gathering
+
+## or update...
+git update -f
+
+time cargo build --bin client --release
+
+echo "Yaay! Next time will be way faster."
