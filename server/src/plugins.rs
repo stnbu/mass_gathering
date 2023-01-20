7887c231f230fe9aac931b5107901e19de5102ec
Mike Burr <mb@unintuitive.org>
2023-01-20 15:49:33 -0700

    cleanup

--- server/src/plugins.rs
+++ server/src/plugins.rs
@@ -11,19 +11,14 @@ impl Plugin for ServerPlugin {
         let args = resources::ServerCliArgs::parse();
         let system = args.system.clone();
         let address = args.address.clone();
-        app
-            // xx
-            .insert_resource(new_renet_server(address))
+        app.insert_resource(new_renet_server(address))
             .insert_resource(args)
             .insert_resource(systems::get_system(&system)())
             .init_resource::<UnassignedMasses>()
             .add_startup_system(populate_unassigned_masses)
             .add_startup_system(setup_physics)
             .add_system(handle_server_events)
-            // yy
-            // Theirs
             .add_plugin(CorePlugin::default())
-            //.add_plugin(PbrPlugin::default())
             .add_plugin(TimePlugin::default())
             .add_plugin(ScheduleRunnerPlugin::default())
             .add_plugin(RenetServerPlugin::default())
