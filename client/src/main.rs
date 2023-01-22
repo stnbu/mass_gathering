5a38460bc253fabb07602e77ecec44fd6a999bb1
Mike Burr <mb@unintuitive.org>
2023-01-22 11:13:31 -0700

    .

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,9 @@
 use bevy::prelude::App;
-use clap::Parser;
-use game::from_nick;
 
 fn main() {
-    let args = game::resources::ClientCliArgs::parse();
-    let address = args.address.clone();
-    let renet_client = client::new_renet_client(from_nick(&args.nickname), address);
     App::new()
         .add_plugin(game::plugins::CorePlugin::default())
         .add_plugin(game::plugins::SimulationPlugin::default())
         .add_plugin(client::plugins::ClientPlugin::default())
-        .insert_resource(renet_client)
         .run();
 }
