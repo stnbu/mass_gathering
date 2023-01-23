f36219f3a36f0eb7eb9b6407497ead8b2e51740f
Mike Burr <mb@unintuitive.org>
2023-01-23 16:14:10 -0700

    YY -- Ok... I know the preveious commit says "BROKEN" but I have not re-encountered that mysterious behavior and the commit message has a pretty good description.

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -13,7 +13,7 @@ pub mod systems;
 
 pub mod plugins;
 
-pub const PROTOCOL_ID: u64 = 32;
+pub const PROTOCOL_ID: u64 = 33;
 pub const SERVER_IP: &str = "127.0.0.1";
 pub const SERVER_PORT: u16 = 5743; // FIXME NOTE -- don't change this anymore
 pub const SQRT_3: f32 = 1.7320508;
