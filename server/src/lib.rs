42de09495017f072fa2d1e8a652ab774cfd144aa
Mike Burr <mb@unintuitive.org>
2023-01-23 10:07:50 -0700

    YYYY ---- very nice. all kinda solid. FYI: server is somewhat tamed CPU-wise, but it "must" have a window (and deps). server also does zero simulation stuff.

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -55,6 +55,17 @@ pub fn setup_physics(mut commands: Commands, cli_args: Res<resources::ServerCliA
     });
 }
 
+/*
+Reasoning about client bootstrapping and stuff:
+
+1)   Client connects
+2)   Server handles `ClientConnected`: Client given: init data, physics config and the lobby "so far".
+2.5) [Race condition?] A client might get added to "Lobby" inside of the "Ready" handler while another
+     is getting the "Lobby so far". Maybe Rust magic prevents this? (mut ref vs immut ref).
+3)   Client sends `Ready`
+4)   Server handles `Ready`: Adds
+*/
+
 pub fn handle_server_events(
     mut server_events: EventReader<ServerEvent>,
     mut server: ResMut<RenetServer>,
@@ -69,52 +80,28 @@ pub fn handle_server_events(
         match event {
             ServerEvent::ClientConnected(id, _) => {
                 let new_client_id = *id;
-                let inhabited_mass_id = if let Some(mass_id) = unassigned_masses.0.pop() {
-                    mass_id
-                } else {
-                    warn!("Got connection from client {new_client_id} but we have no more uninhabited masses");
-                    // FIXME: send "error" back to client?
-                    return;
-                };
-
-                debug!("  sending initial data to client {new_client_id}");
-                let message =
-                    bincode::serialize(&events::ToClient::Init(init_data.clone())).unwrap();
-                server.send_message(new_client_id, DefaultChannel::Reliable, message);
-
-                debug!("  sending physics config to {new_client_id}");
-                let message =
+                server.send_message(
+                    new_client_id,
+                    DefaultChannel::Reliable,
+                    bincode::serialize(&events::ToClient::Init(init_data.clone())).unwrap(),
+                );
+                server.send_message(
+                    new_client_id,
+                    DefaultChannel::Reliable,
                     bincode::serialize(&events::ToClient::SetPhysicsConfig(*physics_config))
-                        .unwrap();
-                server.send_message(new_client_id, DefaultChannel::Reliable, message);
-
-                debug!("  replaying existing lobby back to new client {new_client_id:?}");
+                        .unwrap(),
+                );
                 for (&existing_id, &client_data) in lobby.clients.iter() {
-                    let message = events::ToClient::ClientJoined {
-                        id: existing_id,
-                        client_data,
-                    };
                     server.send_message(
                         new_client_id,
                         DefaultChannel::Reliable,
-                        bincode::serialize(&message).unwrap(),
+                        bincode::serialize(&events::ToClient::ClientJoined {
+                            id: existing_id,
+                            client_data,
+                        })
+                        .unwrap(),
                     );
                 }
-                // FIXME: This probably wants to be under "ToServer::Ready"
-                let client_data = resources::ClientData { inhabited_mass_id };
-                debug!("  now updating my lobby with ({new_client_id}, {client_data:?})");
-                lobby.clients.insert(new_client_id, client_data);
-                debug!("  the server now has lobby {lobby:?}");
-                let message = events::ToClient::ClientJoined {
-                    id: new_client_id,
-                    client_data,
-                };
-                debug!("  broadcasting about new client: {message:?}");
-                // NOTE: This is how the client gets its own entry in its copy of `Lobby`.
-                server.broadcast_message(
-                    DefaultChannel::Reliable,
-                    bincode::serialize(&message).unwrap(),
-                );
             }
             ServerEvent::ClientDisconnected(id) => {
                 debug!(
@@ -127,13 +114,28 @@ pub fn handle_server_events(
     }
 
     for client_id in server.clients_id().into_iter() {
-        // FIXME: What is the "clean" way to "iterate all channels".
         while let Some(message) = server.receive_message(client_id, DefaultChannel::Reliable) {
             let message = bincode::deserialize(&message).unwrap();
             trace!("Received message from client {client_id}: {message:?}");
             match message {
                 events::ToServer::Ready => {
-                    debug!("Got 'Ready' from {client_id}");
+                    let inhabited_mass_id = if let Some(mass_id) = unassigned_masses.0.pop() {
+                        mass_id
+                    } else {
+                        debug!("No more assignable masses");
+                        return;
+                    };
+                    let client_data = resources::ClientData { inhabited_mass_id };
+                    lobby.clients.insert(client_id, client_data);
+                    server.broadcast_message(
+                        DefaultChannel::Reliable,
+                        bincode::serialize(&events::ToClient::ClientJoined {
+                            id: client_id,
+                            client_data,
+                        })
+                        .unwrap(),
+                    );
+
                     let start = lobby.clients.len()
                         == init_data
                             .masses
@@ -141,27 +143,18 @@ pub fn handle_server_events(
                             .filter(|(_, data)| data.inhabitable)
                             .count();
                     if start {
-                        /* PROBLEM! (this should print only once)
-                        yolo:mass_gathering mburr$ less -R /tmp/logs/1674427249/server.out | grep 'capacity'
-                        2023-01-22T22:40:55.953958Z DEBUG server:   game has now reached capacity.
-                        2023-01-22T22:40:55.973865Z DEBUG server:   game has now reached capacity.
-                        2023-01-22T22:40:56.004610Z DEBUG server:   game has now reached capacity.
-                        */
-                        debug!("  game has now reached capacity.");
+                        assert!(unassigned_masses.0.is_empty());
                         let state = resources::GameState::Running;
                         let set_state = events::ToClient::SetGameState(state);
                         let message = bincode::serialize(&set_state).unwrap();
-                        debug!("Broadcasting {set_state:?}");
                         server.broadcast_message(DefaultChannel::Reliable, message);
-                        debug!("  and setting my state to {state:?}");
                         let _ = app_state.overwrite_set(state);
                     } else {
+                        assert!(!unassigned_masses.0.is_empty());
                         let state = resources::GameState::Waiting;
                         let set_state = events::ToClient::SetGameState(state);
                         let message = bincode::serialize(&set_state).unwrap();
-                        debug!("Replying to client {client_id} with {state:?}");
                         server.send_message(client_id, DefaultChannel::Reliable, message);
-                        debug!("  and setting my state to {set_state:?}");
                         let _ = app_state.overwrite_set(state);
                     }
                 }
