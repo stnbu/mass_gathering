c7bf7d49c825f7a1870b28fc7b268492b59c191f
Mike Burr <mb@unintuitive.org>
2023-01-30 14:25:57 -0700

    YYY - sim on server

--- server/src/plugins.rs
+++ server/src/plugins.rs
@@ -28,6 +28,17 @@ impl Plugin for ServerPlugin {
                 init_data: systems::get_system(&args.system)(),
                 ..Default::default()
             })
+            //
+            .add_plugin(game::plugins::SimulationPlugin::default())
+            .add_event::<simulation::FromSimulation>()
+            .add_system_set(
+                SystemSet::on_update(resources::GameState::Running)
+                    .with_system(simulation::handle_projectile_fired)
+                    .with_system(simulation::move_projectiles)
+                    .with_system(simulation::handle_projectile_collision)
+                    .with_system(simulation::rotate_inhabitable_masses),
+            )
+            //
             .add_startup_system(spawn_masses)
             .add_system(panic_on_renet_error)
             .add_system(handle_server_events)
