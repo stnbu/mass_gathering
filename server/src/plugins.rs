2ebfcce594376cb4e76f3870ad1288155e314bcb
Mike Burr <mb@unintuitive.org>
2023-02-01 12:13:46 -0700

    ~~y - the server is going to need some work. we need to conditionally add some features to bevy to make the window actually work (winit and stuff). probably add "window" feature to server.

--- server/src/plugins.rs
+++ server/src/plugins.rs
@@ -14,14 +14,6 @@ impl Plugin for ServerPlugin {
 
         app
             //
-            .add_plugins(MinimalPlugins)
-            // FIXME: As per https://github.com/dimforge/bevy_rapier/issues/296
-            // manually adding some stuff. Is there a problem with this? Can/should
-            // I use the `headless` feature of `bevy_rapier`?
-            .add_plugin(AssetPlugin::default())
-            .add_asset::<Mesh>()
-            .add_asset::<Scene>()
-            .add_plugin(get_log_plugin("server"))
             .insert_resource(new_renet_server(address))
             .insert_resource(resources::GameConfig {
                 physics_config: resources::PhysicsConfig { speed, zerog },
@@ -42,6 +34,7 @@ impl Plugin for ServerPlugin {
             .add_startup_system(spawn_masses)
             .add_system(panic_on_renet_error)
             .add_system(handle_server_events)
+            .add_plugins(DefaultPlugins.set(get_log_plugin("server")))
             .add_plugin(RenetServerPlugin::default())
             .run();
     }
