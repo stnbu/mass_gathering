b2f224b8a03ec73f347376b0f7d3b81e1738768e
Mike Burr <mb@unintuitive.org>
2023-02-09 11:41:29 -0700

    more "systems" stuff

--- game/src/systems/mod.rs
+++ game/src/systems/mod.rs
@@ -1,3 +1,8 @@
+fn gah() {
+    let mut x = 123u32;
+    x = -1;
+}
+
 use crate::*;
 use std::collections::HashMap;
 
@@ -76,7 +81,7 @@ pub fn demo_shooting() -> resources::InitData {
     init_data
 }
 
-pub fn old_rando() -> resources::InitData {
+pub fn rando_calrissian() -> resources::InitData {
     resources::InitData {
         masses: HashMap::from([
             // Uninhabitable masses:
@@ -228,7 +233,7 @@ pub fn old_rando() -> resources::InitData {
             (
                 2037,
                 resources::MassInitData {
-                    inhabitable: true,
+                    inhabitable: false,
                     motion: resources::MassMotion {
                         position: Vec3::new(1e-6, 70.0, 1e-6),
                         velocity: Vec3::new(1e-6, 1e-6, 1e-6),
@@ -276,7 +281,7 @@ pub fn old_rando() -> resources::InitData {
             (
                 2038,
                 resources::MassInitData {
-                    inhabitable: true,
+                    inhabitable: false,
                     motion: resources::MassMotion {
                         position: Vec3::new(1e-6, 1e-6, 70.0),
                         velocity: Vec3::new(1e-6, 1e-6, 1e-6),
@@ -553,9 +558,88 @@ pub fn old_rando() -> resources::InitData {
     }
 }
 
+pub fn cubic() -> resources::InitData {
+    let mut init_data = resources::InitData::default();
+
+    let mut mass_id = 2000;
+    let from_origin = 9.0;
+    for n in [(1, 0, 0), (0, 1, 0), (0, 0, 1)] {
+        for side in [1.0, -1.0] {
+            let fun_factor = 1.0 + (mass_id as f32 - 2000.0) / 20.0;
+
+            let (a, b, c) = n;
+            let speed = 0.15;
+            let position = Vec3::new(
+                a as f32 * side * from_origin,
+                b as f32 * side * from_origin,
+                c as f32 * side * from_origin,
+            );
+            let velocity = match (a, b, c) {
+                (1, 0, 0) => Vec3::Y * side,
+                (0, 1, 0) => Vec3::Z * side,
+                (0, 0, 1) => Vec3::X * side,
+                _ => panic!(),
+            } * speed;
+            let (r, g, b) = (a as f32, b as f32, c as f32);
+            let plus_side = side > 0.0;
+            let color = if plus_side {
+                [r, g, b, 0.8]
+            } else {
+                [(1.0 - r) / 2.0, (1.0 - g) / 2.0, (1.0 - b) / 2.0, 0.8]
+            };
+            let velocity = if c == 1 {
+                velocity
+            } else {
+                velocity * fun_factor
+            };
+            let mass = radius_to_mass(if a == 1 { 0.5 } else { 0.5 * fun_factor });
+
+            let position = if c == 1 {
+                position
+            } else {
+                position * fun_factor
+            };
+
+            let mass_init_data = resources::MassInitData {
+                inhabitable: false,
+                motion: resources::MassMotion { position, velocity },
+                color,
+                mass,
+            };
+            init_data.masses.insert(mass_id, mass_init_data);
+            mass_id += 1;
+        }
+    }
+
+    let inhabitable_distance = 20.0;
+    for (x, y, z) in [(1, 0, 0), (0, 1, 0), (0, 0, 1)] {
+        let velocity = Vec3::ZERO;
+        let color_tweak = match (x, y, z) {
+            (1, 0, 0) => 1.0,
+            (0, 1, 0) => 2.0,
+            (0, 0, 1) => 3.0,
+            _ => panic!("no!"),
+        };
+        let position = Vec3::new(x as f32, y as f32, z as f32) * inhabitable_distance;
+        let color = [17.0, 19.0 / color_tweak, 23.0 * color_tweak, 1.0];
+        let mass = radius_to_mass(1.0);
+        let inhabitable = z == 1;
+        let mass_init_data = resources::MassInitData {
+            inhabitable,
+            motion: resources::MassMotion { position, velocity },
+            color,
+            mass,
+        };
+        init_data.masses.insert(mass_id, mass_init_data);
+        mass_id += 1;
+    }
+    init_data
+}
+
 pub fn get_system(name: &str) -> impl (Fn() -> resources::InitData) {
     match name {
-        "old_rando" => old_rando,
+        "cubic" => cubic,
+        "rando_calrissian" => rando_calrissian,
         "demo_2m2i" => demo_2m2i,
         "demo_shooting" => demo_shooting,
         _ => panic!("No such system: {name}"),
